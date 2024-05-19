9dc6206b1129cae0e2432fbedbe60575a008be82
Krasnomakov <krasnomakov@gmail.com>
2024-05-19 11:55:04 +0200

    cameraTest

--- Ellie_connected_v3/cameraTest.py
+++ Ellie_connected_v3/cameraTest.py
@@ -1,18 +1,39 @@
 import cv2
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+class VideoAnimation:
+    def __init__(self):
+        self.pixelated_width, self.pixelated_height = 20, 20
+        self.display_width, self.display_height = 400, 400
+        self.lower_color = np.array([100, 150, 150])
+        self.upper_color = np.array([140, 255, 255])
+        self.cap = None
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    def open_camera(self):
+        cap = cv2.VideoCapture(0)
+        if cap.isOpened():
+            return cap
+        print("[ERROR] Unable to open camera at /dev/video0.")
+        return None
 
-cap.release()
-cv2.destroyAllWindows()
+    def run(self):
+        self.cap = self.open_camera()
+        if not self.cap:
+            print("[ERROR] Camera could not be opened.")
+            return
+        
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+
+            cv2.imshow('Camera Test', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    animation = VideoAnimation()
+    animation.run()
